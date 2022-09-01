b59756c6fa21b5538b6fb060943e5892aca15661
Tanvi <tanvidhingra2000@gmail.com>
2022-09-01 10:02:14 +0530

    Random Function

---
+++ src/Sample.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Sample
+{
+    public static void main(String [] args)
+    {
+        Random random = new Random();
+        System.out.println(random.nextInt(1000));
+    }
+}
